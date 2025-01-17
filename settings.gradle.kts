3a44e608c5228648bdb0e39ff1837268fa9d96cd
runner <runner@hmpps-github-actions-runner-59bd8685f7-pvzk8>
2025-01-17 15:12:01 +0000

    Rename project

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "hmpps-template-kotlin"
+rootProject.name = "hmpps-managing-prisoner-apps-api"
--- src/main/kotlin/uk/gov/justice/digital/hmpps/managingprisonerappsapi/ManagingPrisonerAppsApi.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/managingprisonerappsapi/ManagingPrisonerAppsApi.kt
@@ -1,11 +1,11 @@
-package uk.gov.justice.digital.hmpps.templatepackagename
+package uk.gov.justice.digital.hmpps.managingprisonerappsapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class HmppsTemplateKotlin
+class ManagingPrisonerAppsApi
 
 fun main(args: Array<String>) {
-  runApplication<HmppsTemplateKotlin>(*args)
+  runApplication<ManagingPrisonerAppsApi>(*args)
 }
