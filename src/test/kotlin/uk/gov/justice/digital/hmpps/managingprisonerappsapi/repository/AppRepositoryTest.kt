debcfeb3585fe991e0dbe3356ff263746c6191a2
Varun Kumar <varun.kumar@digital.justice.gov.uk>
2025-01-27 15:14:34 +0000

    adding spring boo test annotations

--- src/test/kotlin/uk/gov/justice/digital/hmpps/managingprisonerappsapi/repository/AppRepositoryTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/managingprisonerappsapi/repository/AppRepositoryTest.kt
@@ -1,4 +1,18 @@
 package uk.gov.justice.digital.hmpps.managingprisonerappsapi.repository
 
-class AppRepositoryTest {
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.boot.autoconfigure.domain.EntityScan
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import uk.gov.justice.digital.hmpps.managingprisonerappsapi.model.App
+
+@SpringBootTest(classes = [App::class])
+@EnableAutoConfiguration
+@EnableJpaRepositories(basePackages = ["uk.gov.justice.digital.hmpps.managingprisonerappsapi.repository"])
+@EntityScan("uk.gov.justice.digital.hmpps.managingprisonerappsapi.model")
+@ExtendWith(SpringExtension::class)
+class AppRepositoryTest(@Autowired val appRepository: AppRepository) {
 }
