f1067bb5b42abebb3a14b8eab82ff16902d1f089
Varun Kumar <varun.kumar@digital.justice.gov.uk>
2025-05-14 13:24:21 +0100

    code clean up

--- src/main/resources/db/migration/default/V004__comment_table.sql
+++ src/main/resources/db/migration/default/V004__comment_table.sql
@@ -1,9 +1,9 @@
 create table if not exists public.comment
 (
-    created_date timestamp(6),
-    app          uuid,
     id           uuid not null
     primary key,
+    created_date timestamp(6),
+    app          uuid,
     message      varchar(1000),
     created_by   varchar(255)
     );
