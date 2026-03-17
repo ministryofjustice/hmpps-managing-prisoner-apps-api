e6ffd4de9a73382fb1ac5e8cd50f133cebba2c0e
Varun Kumar <varun.kumar@digital.justice.gov.uk>
2026-03-17 09:47:55 +0000

    expose api to get prisoner apps

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/managingprisonerappsapi/dto/response/AppListPrisonerFacing.kt
@@ -0,0 +1,14 @@
+package uk.gov.justice.digital.hmpps.managingprisonerappsapi.dto.response
+
+import uk.gov.justice.digital.hmpps.managingprisonerappsapi.model.AppStatus
+import java.time.LocalDateTime
+import java.util.UUID
+
+data class AppListPrisonerFacing(
+  val id: UUID,
+  val prisonerId: String,
+  val applicationType: String,
+  val createdDate: LocalDateTime,
+  val lastUpdatedDate: LocalDateTime,
+  val status: AppStatus,
+)
