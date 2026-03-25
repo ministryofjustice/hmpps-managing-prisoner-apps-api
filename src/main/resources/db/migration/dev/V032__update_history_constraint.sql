150f4a698e3954745cf76c5632b102464a19bbff
Varun kumar <134276773+varunkumar139@users.noreply.github.com>
2026-03-25 14:32:33 +0000

    updating history table constraint (#131)

---
+++ src/main/resources/db/migration/dev/V032__update_history_constraint.sql
@@ -0,0 +1,31 @@
+ALTER TABLE if exists history DROP CONSTRAINT if exists history_activity_check;
+
+ALTER TABLE if exists history ADD CONSTRAINT history_activity_check
+    check ((activity)::text = ANY
+    ((ARRAY
+    [
+    'APP_SUBMITTED':: character varying,
+    'APP_REQUEST_FORM_DATA_UPDATED':: character varying,
+    'APP_FORWARDED_TO_A_GROUP':: character varying,
+    'COMMENT_ADDED':: character varying,
+    'FORWARDING_COMMENT_ADDED':: character varying,
+    'RESPONSE_ADDED':: character varying,
+    'APP_APPROVED':: character varying,
+    'APP_DECLINED':: character varying,
+    'FILE_ADDED':: character varying
+    ]
+    )::text[]));
+
+ALTER TABLE if exists history DROP CONSTRAINT if exists history_entity_type_check;
+
+ALTER TABLE if exists history ADD CONSTRAINT history_entity_type_check
+    check ((entity_type)::text = ANY
+    ((ARRAY
+    [
+    'APP':: character varying,
+    'COMMENT':: character varying,
+    'RESPONSE':: character varying,
+    'ASSIGNED_GROUP':: character varying,
+    'FILE':: character varying
+    ]
+    )::text[]));
