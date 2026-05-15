ba4462a899cafebec1ff59e1bce8724944919708
BOBY JACOB <Boby.Jacob@justice.gov.uk>
2026-05-15 08:10:58 +0100

    SAR-Remove redundant file

--- src/main/kotlin/uk/gov/justice/digital/hmpps/managingprisonerappsapi/service/integration/client/HmppsAuthAccessToken.kt
+++
@@ -1,8 +0,0 @@
-package uk.gov.justice.digital.hmpps.managingprisonerappsapi.service.integration.client
-
-import com.fasterxml.jackson.annotation.JsonProperty
-
-data class HmppsAuthAccessToken(
-  @JsonProperty("access_token")
-  val accessToken: String,
-)
